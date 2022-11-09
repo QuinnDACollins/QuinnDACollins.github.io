4821fb5ebe6e68e1d751c66adaa94a34a2c8706e
Quinn Collins <quinndacollins@gmail.com>
2022-11-09 10:44:26 -0600

    Remove scroll overflow, update playlists

--- index.js
+++ index.js
@@ -11,26 +11,36 @@ $(document).ready(function () {
         columnWidth: '.grid-sizer',
         percentPosition: true
     });
+
+    // let file = figure out how to get files from directory
+    let reader = new FileReader();
+
+    reader.readAsText(file);
+
+    reader.onload = function () {
+        console.log(reader.result);
+    }
 });
 
-function getSpotifyPlaylists() {
-    var resp = fetch(`https://accounts.spotify.com/api/token`, {
-            body: `grant_type=client_credentials`,
-            method: 'post',
+/* function getSpotifyPlaylists() {
+var resp = fetch(`https://accounts.spotify.com/api/token`, {
+        body: `grant_type=client_credentials`,
+        method: 'post',
+        headers: {
+            'Authorization': `Basic ${btoa("clientid:clientsecret")}`,
+            'Content-Type': "application/x-www-form-urlencoded"
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        var playlistResp = fetch(`${APILINKS.SPOTIFY}/users/${MY_IDS.SPOTIFY}/playlists`, {
             headers: {
-                'Authorization': `Basic ${btoa("clientid:clientsecret")}`,
-                'Content-Type': "application/x-www-form-urlencoded"
+                'Authorization': `Bearer ${data.access_token}`,
+                'Content-Type': "application/json"
             }
         })
-        .then(response => response.json())
-        .then(data => {
-            var playlistResp = fetch(`${APILINKS.SPOTIFY}/users/${MY_IDS.SPOTIFY}/playlists`, {
-                headers: {
-                    'Authorization': `Bearer ${data.access_token}`,
-                    'Content-Type': "application/json"
-                }
-            })
-        })
+    })
 
 
 }
+*/
