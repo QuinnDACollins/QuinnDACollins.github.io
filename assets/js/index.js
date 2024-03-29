18ce808aa90a8337e713dd36035ca878b87620e3
Quinn <QuinnDaCollins@gmail.com>
2024-03-29 02:01:26 -0700

    szdfg

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,7 +13,7 @@ function updateDescription(element) {
     var who = document.getElementById("post-" + ctx + "-who")
     document.getElementById("collection-title").textContent = title.textContent;
     document.getElementById("collection-description").textContent = description.textContent;
-    document.getElementById("collection-who").textContent = who.textContent;
+    document.getElementById("collection-who").textContent = "Requested by " + who.textContent;
     document.getElementById("collection-box").classList.remove("animate__slideOutRight");
     document.getElementById("collection-box").classList.add("animate__slideInRight");
 }
