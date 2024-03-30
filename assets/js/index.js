4519c0196b790d3c4c27185422c3982729ee4800
Quinn <QuinnDaCollins@gmail.com>
2024-03-30 11:45:45 -0700

    posts, credits, bike lock

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,9 +11,11 @@ function updateDescription(element) {
     var title = document.getElementById("post-" + ctx + "-title")
     var description = document.getElementById("post-" + ctx + "-description")
     var who = document.getElementById("post-" + ctx + "-who")
+    var visuals = document.getElementById("post-" + ctx + "-visuals")
     document.getElementById("collection-title").textContent = title.textContent;
     document.getElementById("collection-description").textContent = description.textContent;
     document.getElementById("collection-who").textContent = "Requested by " + who.textContent;
+    document.getElementById("collection-visuals").href = visuals.textContent;
     document.getElementById("collection-box").classList.remove("animate__slideOutRight");
     document.getElementById("collection-box").classList.add("animate__slideInRight");
 }
